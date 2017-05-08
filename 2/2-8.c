2d8ea5c1163973d5817dfc97ebea72d718fcdcf6
mingyunyuansu <mingyunyuansu@163.com>
2017-05-08 21:33:13 +0800

    finished 2-8 P40

--- 2/2-8.c
+++ 2/2-8.c
@@ -3,10 +3,10 @@
 unsigned rightrot(unsigned x, int n);
 
 int main(int argc, char ** argv) {
-    printf("%d\n", rightrot(atoi(argv[1]), atoi(argv[2])));
+    printf("%x to %x\n", atoi(argv[1]), rightrot(atoi(argv[1]), atoi(argv[2])));
     return 0;
 }
 
 unsigned rightrot(unsigned x, int n) { 
-    return ((~(~0<<n) & x) << (sizeof(x) * 8 - n)) | ((~(~0 << n)) & x);
+    return ((~((~0)<<n) & x) << (sizeof(x) * 8 - n)) | ((x>>n) & ~(~0<<(sizeof(x)*8 - n)));
 }
